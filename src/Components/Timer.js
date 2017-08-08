f44713adce24d3bc7911e19febd8a79427c21d94
Daniel Lemay <d.s.lemay@icloud.com>
2017-08-08 13:47:38 -0700

    Add basic countdown method
    The countdownTimer method has been added and will now begin running
    when isRunning becomes true. There are still some bugs and and
    additional work to be completed.
    
    TODO:
    
    * Fix glitch that doesn't display first state change: immediately jumps
    to 58 seconds
    * Create logic to stop the timer
    * Create logic to know which timer is active

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -10,7 +10,9 @@ class Timer extends React.Component {
     this.state = {
       // countdown timers are stored in state in ms as they are directly manipulated by the timer method.
       sessionCountdown: props.sessionTime * 60000,
-      breakCountdown: props.breakTime * 60000
+      breakCountdown: props.breakTime * 60000,
+      minutes: props.sessionTime,
+      seconds: "00"
     }
   }
 
@@ -23,6 +25,7 @@ class Timer extends React.Component {
     }
     if (nextProps.isRunning === true) {
       console.log("Timer starting!");
+      this.countdownTimer();
     }
     if (nextProps.isRunning === false) {
       console.log("Timer paused.");
@@ -30,14 +33,26 @@ class Timer extends React.Component {
   }
 
   countdownTimer() {
-
+    var endTime = Date.now() + this.state.sessionCountdown;
+    setInterval(() => {
+      var timeLeft = endTime - Date.now();
+      var minutesLeft = Math.floor(timeLeft/60000 % 60);
+      var secondsLeft = Math.floor(timeLeft/1000 % 60);
+      if (secondsLeft < 10) {
+        secondsLeft = "0" + String(secondsLeft);
+      }
+      this.setState({
+        minutes: minutesLeft,
+        seconds: secondsLeft
+      });
+    }, 1000);
   }
 
   render() {
 
     return (
       <div id="timer-countdown">
-
+        {this.state.minutes} : {this.state.seconds}
       </div>
     )
   }
