7a96158e2b5e61f9d92c2606278d6e486e2f4a7c
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2018-08-04 10:52:03 -0700

    Add babel-eslint parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  parser: 'babel-eslint',
   env: {
     browser: true,
     es6: true,
@@ -29,7 +30,8 @@ module.exports = {
       'error',
       {
         singleQuote: true,
-        printWidth: 100
+        printWidth: 100,
+        trailingComma: 'all',
       }
     ]
   }
